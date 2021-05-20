10126276239a88810d7ac0bcd3dfad5e407ed77a
kctang97 <kaicong.tang@sjsu.edu>
2021-05-19 17:22:47 -0700

    add side menu

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function openSideMenu(){
-  document.getElementById('sideMenu').style.width = '250px';
-  document.getElementById('main').style.marginLeft = '250px';
-}
-
-function closeSideMenu(){
-  document.getElementById('sideMenu').style.width = '0';
-  document.getElementById('main').style.marginLeft = '0';
-}
+// function openSideMenu(){
+//   document.getElementById('sideMenu').style.width = '250px';
+//   document.getElementById('main').style.marginLeft = '250px';
+// }
+//
+// function closeSideMenu(){
+//   document.getElementById('sideMenu').style.width = '0';
+//   document.getElementById('main').style.marginLeft = '0';
+// }
